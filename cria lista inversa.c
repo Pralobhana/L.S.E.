827ba91ab737c694ba5aeb7d90e21778a23d32bb
Lívia Ferrete <44952246+Pralobhana@users.noreply.github.com>
2018-11-11 19:17:39 -0200

    Add files via upload

---
+++ cria lista inversa.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+struct ptr 
+{
+	int valor;
+	struct ptr *prox;
+};
+struct ptr *inicio;
+void cria_lista();
+int main()
+{
+	cria_lista();
+}
+void cria_lista()
+{
+	int valor;
+	struct ptr *aux;
+	inicio = (struct ptr *)NULL;
+	printf("ENTRE COM O VALOR: ");
+	scanf("%d", &valor);
+	while(valor >= 0)
+	{
+		aux = (struct ptr *)malloc(sizeof(struct ptr));
+		aux->valor = valor;
+		aux->prox = inicio;
+		inicio = aux;
+		printf("ENTRE COM O VALOR: ");
+		scanf("%d", &valor);
+	}
+	aux = (struct ptr *)NULL;
+}
