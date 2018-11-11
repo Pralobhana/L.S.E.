cc518bb9c90d7c59d4a7823a0aa63b705494f64e
Lívia Ferrete <44952246+Pralobhana@users.noreply.github.com>
2018-11-11 20:38:18 -0200

    Update mostra lista recursão.c

--- mostra lista recursão.c
+++ mostra lista recursão.c
@@ -20,7 +20,7 @@ void cria_lista_inv(struct ptr *aux)
     printf("ENTRE COM O VALOR: ");
     scanf("%d", &valor);
     if(valor >= 0) 
-	{
+    {
         aux = (struct ptr *)malloc(sizeof(struct ptr));
         aux->valor = valor;
         aux->prox = inicio;
@@ -34,9 +34,9 @@ void mostra(struct ptr *aux)
     if(inicio == (struct ptr *)NULL)
         printf("LISTA VAZIA!");
     else 
-	{
+    {
         if(aux != (struct ptr *)NULL) 
-		{
+	{
             printf("%d\t", aux->valor);
             aux = aux->prox;
             mostra(aux);
