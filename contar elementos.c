f549351951435db4db1d5a58ad53064b83186969
Lívia Ferrete <44952246+Pralobhana@users.noreply.github.com>
2018-11-11 21:14:00 -0200

    Add files via upload

---
+++ contar elementos.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+struct ptr 
+{
+	int valor;
+	struct ptr *prox;
+};
+struct ptr *inicio;
+void cria_lista_inv();
+void conta();
+int main()
+{
+	cria_lista_inv();
+	conta();
+}
+void cria_lista_inv()
+{
+	struct ptr *aux;
+	int valor;
+	inicio = (struct ptr *)NULL;
+	printf("ENTRE COM O VALOR: ");
+	scanf("%d", &valor);
+	while(valor >= 0)
+	{
+		aux = (struct ptr *)malloc(sizeof(struct ptr));
+		aux->valor = valor;
+		aux->prox = inicio;
+		inicio = aux;
+		printf("ENTRE COM O VALOR: ");
+		scanf("%d", &valor);
+	}
+	aux = (struct ptr *)NULL;
+}
+void conta()
+{
+	struct ptr *aux;
+	int cont = 0;
+	aux = inicio;
+	while (aux != (struct ptr *)NULL)
+	{
+		cont++;
+		aux = aux->prox;
+	}
+	printf("\nTOTAL DE ELEMENTOS: %d", cont);
+}
