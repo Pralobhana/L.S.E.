48abb80d50e8c4efcbadc0704a6b6b1c40ee6bf6
Lívia Ferrete <44952246+Pralobhana@users.noreply.github.com>
2018-11-11 19:31:20 -0200

    Add files via upload

---
+++ mostra lista recursão.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+struct ptr 
+{
+    int valor;
+    struct ptr *prox;
+};
+struct ptr *inicio, *aux;
+void cria_lista_inv(struct ptr *aux);
+void mostra(struct ptr *aux);
+int main()
+{
+    cria_lista_inv((struct ptr *)NULL);
+    mostra(inicio);
+}
+void cria_lista_inv(struct ptr *aux)
+{
+    int valor;
+    if(aux == (struct ptr *)NULL)
+        inicio = (struct ptr *)NULL;
+    printf("ENTRE COM O VALOR: ");
+    scanf("%d", &valor);
+    if(valor >= 0) 
+	{
+        aux = (struct ptr *)malloc(sizeof(struct ptr));
+        aux->valor = valor;
+        aux->prox = inicio;
+        inicio = aux;
+        cria_lista_inv(aux);
+    }
+    aux = (struct ptr *)NULL;
+}
+void mostra(struct ptr *aux)
+{
+    if(inicio == (struct ptr *)NULL)
+        printf("LISTA VAZIA!");
+    else 
+	{
+        if(aux != (struct ptr *)NULL) 
+		{
+            printf("%d\t", aux->valor);
+            aux = aux->prox;
+            mostra(aux);
+        }
+    }
+}
---
+++ mostra lista.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+struct ptr 
+{
+	int valor;
+	struct ptr *prox;
+};
+struct ptr *inicio;
+void cria_lista();
+void mostra();
+int main()
+{
+	cria_lista();
+	mostra();
+}
+void cria_lista()
+{
+	int valor;
+	struct ptr *aux;
+	inicio = (struct ptr *)NULL;
+	printf("ENTRE COM O VALOR: ");
+	scanf("%d", &valor);
+	while(valor >= 0)
+	{
+		aux = (struct ptr *)malloc(sizeof(struct ptr));
+		aux->valor = valor;
+		aux->prox = inicio;
+		inicio = aux;
+		printf("ENTRE COM O VALOR: ");
+		scanf("%d", &valor);
+	}
+	aux = (struct ptr *)NULL;
+}
+void mostra()
+{
+	struct ptr *aux;
+	if(inicio == (struct ptr *)NULL)
+		printf("LISTA VAZIA!");
+	else
+	{
+		aux = inicio;
+		while(aux != (struct ptr *)NULL)
+		{
+			printf("%d\t", aux->valor);
+			aux=aux->prox;
+		}
+	}
+}
